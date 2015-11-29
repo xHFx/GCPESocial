94f08e47eaba5c4da2200863cfd2e5a5bc7165b7
TheDeibo <deiboibbs@gmail.com>
2015-11-29 21:45:11 +0000

    Update Main.php

--- src/GameCraftPE/Main.php
+++ src/GameCraftPE/Main.php
@@ -8,11 +8,9 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\Player;
 
-/** Im adding a few more "use".
- * Basically, i need to delete the comment lines when done.
 use GameCraftPE\config\website;
 use GameCraftPE\config\github;
-use GameCraftPE\config\twitter;
+use GameCraftPE\config\facebook;
 use GameCraftPE\config\youtube;
 **/
 class Main extends PluginBase implements Listener{
@@ -39,18 +37,11 @@ class Main extends PluginBase implements Listener{
                 }
                 $player = $this->getServer()->getPlayer($sender->getName());
                 if ($player->hasPermission("gamecraftpe.staff")){
-                    $sender->sendMessage("§a"--------[GCPESocial]--------);
-                    $sender->sendMessage("§2- " . $this->getConfig()->get("admin1"));
-                    $sender->sendMessage("§a- " . $this->getConfig()->get("admin2"));
-                    $sender->sendMessage("§a--------§2[GameCraftPE Developpers]§a--------");
-                    $sender->sendMessage("§a- " . $this->getConfig()->get("developper4"));
-                    $sender->sendMessage("§2- " . $this->getConfig()->get("developper5"));
-                    $sender->sendMessage("§a--------§2[GameCraftPE Staff]§a--------");
-                    $sender->sendMessage("§2- " . $this->getConfig()->get("staff1"));
-                    $sender->sendMessage("§a- " . $this->getConfig()->get("staff2"));
-                    $sender->sendMessage("§2- " . $this->getConfig()->get("staff3"));
-                    $sender->sendMessage("§a- " . $this->getConfig()->get("staff4"));
-                    $sender->sendMessage("§2- " . $this->getConfig()->get("staff5"));
+                    $sender->sendMessage("§3"--------[GCPESocial]--------);
+                    $sender->sendMessage("§2-  Our Website!);
+                    $sender->sendMessage("§a-  Use /website);
+                    $sender->sendMessage("§2-  Our Website!);
+                    $sender->sendMessage("§a-  Use /website);                    
                     return true;
                 }
                 break;
